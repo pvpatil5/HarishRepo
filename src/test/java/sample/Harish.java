ed8fbf531f209c9b2ddd6e741269245cc6a5f236
Harish246 <srihari224466@gmail.com>
2023-01-12 16:48:24 +0530

    new hyd

--- src/test/java/sample/Harish.java
+++ src/test/java/sample/Harish.java
@@ -5,6 +5,10 @@ public class Harish {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("harish kumar");
+		
+		System.out.println("hyderabad");
+		
+		System.out.println(" new hyderabad");
 
 	}
 
